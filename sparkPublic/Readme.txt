ccc6ec4bd44001b4b11c1e22168d59b53bee2c07
mista-j <johnnie.a.hernandez@gmail.com>
2019-02-23 19:13:06 -0500

    spark update

---
+++ sparkPublic/Readme.txt
@@ -0,0 +1,6 @@
+Landing page for the ECU Spark 2019 event. Created using the bootstrap theme detailed below.
+
+Theme Name: TheEvent
+Theme URL: https://bootstrapmade.com/theevent-conference-event-bootstrap-template/
+Author: BootstrapMade.com
+Author URL: https://bootstrapmade.com
---
+++ sparkPublic/js/timer.js
@@ -0,0 +1,19 @@
+var eventDate = new Date("Apr 6, 2019 8:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = eventDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "<h2>Time Is Up!</h2>";
+  }
+}, 1000);
